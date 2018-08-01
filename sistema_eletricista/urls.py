ec6e846c8459c917cc3f6eb101fdb863a8de127d
Pedro Medeiros <pedro_hmc123@hotmail.com>
2018-08-01 20:25:14 -0300

    arquivos estaticos arrumados

--- sistema_eletricista/urls.py
+++ sistema_eletricista/urls.py
@@ -6,10 +6,15 @@ import sistema_eletricista.apps.user.urls
 from sistema_eletricista import settings
 from django.conf.urls.static import static
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 urlpatterns = [
     url('user/', include('sistema_eletricista.apps.user.urls')),
     url('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#urlpatterns += staticfiles_urlpatterns
 
