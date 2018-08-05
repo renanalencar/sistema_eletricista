461c09382e7766915e1766a484cbac343cd9ddbc
viniciusroland <vini_roland4.10@hotmail.com>
2018-08-05 17:47:05 -0300

    atualizando o staticfiles_dirs

--- sistema_eletricista/settings.py
+++ sistema_eletricista/settings.py
@@ -118,6 +118,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
