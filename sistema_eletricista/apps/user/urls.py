b25fc3a03711a1865ab46c505d37c223512f1b89
Paulo Machado <paulo.machado@polijunior.com.br>
2018-08-15 11:56:06 -0300

    recuperar senha e front login

--- sistema_eletricista/apps/user/urls.py
+++ sistema_eletricista/apps/user/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout_then_login
+from django.contrib.auth import views as auth_views
 from .views import RegistrarEletricistaView
 from .views import QuestionarioView
 from .views import *
@@ -16,8 +17,14 @@ urlpatterns = [
 	url(r'^logout/$', logout_then_login, {'login_url':'/user/login/'}, name='logout'),
 	url(r'^index/$', views.index, name='index'),
 	url(r'^registrar/$', RegistrarEletricistaView.as_view(), name="registrar"),
+	url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 	url(r'^questionario/(?P<nome_eletricista>\w+)/$', QuestionarioView.as_view(), name='questionario'),
 	url(r'^adm/$', views.adm, name='adm'),
+	url(r'^adm/alterar-senha/$', views.change_password, name='change_password'),
 	url(r'^adm/questionarios_pendentes$', views.questionarios_pendentes, name='questionarios_pendentes'),
 	url(r'^adm/aceitar/(?P<nickname>\w+)/$', views.aceitar, name='aceitar'),
 	url(r'^adm/recusar/(?P<nickname>\w+)/$', views.recusar, name='recusar'),
