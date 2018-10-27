c07184b23127ee279f29ac284113f44eecff4c7b
Vinicius Roland <vinicius.roland@polijunior.com.br>
2018-10-27 15:33:43 -0300

    .

--- sistema_eletricista/settings.py
+++ sistema_eletricista/settings.py
@@ -187,7 +187,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('eletricista24hs.polijunior.com.br', 6379)],
         },
     },
 }
