b3ba4497aa417d8092f04b2a771400f6e7a97b83
viniciusroland <vini_roland4.10@hotmail.com>
2018-11-12 12:36:31 -0200

    .

--- sistema_eletricista/wsgi.py
+++ sistema_eletricista/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistema_eletricista.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistema_eletricista.production")
 
 application = get_wsgi_application()
